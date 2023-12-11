0f0e8e43222125860d90f186c8a2681c8b509907
Amrit <amritgagra200@gmail.com>
2023-12-11 17:52:44 +0530

    create about-us page

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,27 @@ $(window).scroll(function(){
        $('.header').removeClass('active');
    }
 });
+
+let target = document.querySelector(".about-team");
+let observer = new IntersectionObserver(entries => {
+    function counter(id, start, end, duration) {
+      let obj = document.querySelector(id),
+       current = start,
+       range = end - start,
+       increment = end > start ? 1 : -1,
+       step = Math.abs(Math.floor(duration / range)),
+       timer = setInterval(() => {
+        current += increment;
+        obj.textContent = `${current}+`;
+        if (current == end) {
+         clearInterval(timer);
+        }
+       }, step);
+     }
+    counter(".counter1", 0, 20, 3000);
+    counter(".counter2", 0, 50, 3000);
+    counter(".counter3", 0, 100, 3000);
+    counter(".counter4", 0, 20, 3000);
+ });
+ 
+ observer.observe(target);
